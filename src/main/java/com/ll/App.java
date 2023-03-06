25abf634dd16d8f73b498bc41d40adda70dbfc7c
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-06 22:01:28 +0900

    명언 수정 기능 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,21 +1,22 @@
 package com.ll;
 
+import com.ll.controller.WiseSayingController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-    private List<WiseSaying> wiseSayings;
+    private WiseSayingController wiseSayingController;
+
     public App(Scanner sc) {
         this.sc = sc;
-        this.wiseSayings = new ArrayList<>();
+        this.wiseSayingController = new WiseSayingController(sc);
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-
-        long lastWiseSayingId = 0;
 
         while(true) {
             System.out.print("명령) ");
@@ -29,50 +30,21 @@ public class App {
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
                 case "등록":
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine().trim();
-                    System.out.print("작가 : ");
-                    String authorName = sc.nextLine().trim();
-
-                    long id = ++lastWiseSayingId;
-
-                    wiseSayings.add(new WiseSaying(id, content, authorName));
-
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    wiseSayingController.write();
                     break;
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    for(int i=wiseSayings.size()-1; i>=0; i--) {
-                        WiseSaying wiseSaying = wiseSayings.get(i);
-                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
-                    }
+                    wiseSayingController.list();
                     break;
                 case "삭제":
-                    remove(rq);
+                    wiseSayingController.remove(rq);
+                    break;
+                case "수정":
+                    wiseSayingController.modify(rq);
                     break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
                     break;
             }
         }
-    }
-    public void remove(Rq rq) {
-        long id = rq.getLongParam("id", -1);
-
-        if(id == -1) {
-            System.out.println("id(정수)를 입력해주세요.");
-            return;
-        }
-
-        for(WiseSaying wiseSaying : wiseSayings) {
-            if(wiseSaying.getId() == id) {
-                wiseSayings.remove(wiseSaying);
-                System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
-                return;
-            }
-        }
-        System.out.printf("%d번 명언이 존재하지 않습니다.\n", id);
     }
 }
